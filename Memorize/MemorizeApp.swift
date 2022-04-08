9880656d23dd9be761adbf9e242696922a4e5b3e
Teiresias22 <teiresias1987@gmail.com>
2022-04-08 21:52:22 +0900

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Joonhwan Jeon on 2022/04/08.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
